6fad1b9ce8f76bbb8bb48f029d44edff4ac9249c
mikayledesp <mdespaigne@smith.edu>
2024-02-06 15:19:00 -0500

    Added the attributes

--- computer.py
+++ computer.py
@@ -1,10 +1,19 @@
 class Computer:
 
     # What attributes will it need?
-
-    # How will you set up your constructor?
+    #  - updating a computer's OS
+    operating_system: str
+    #  - storing information about a specific computer
+    description: str
+    processor_type: str
+    hard_drive_capacity: str 
+    memory:str 
+    year_made: int
+    price: int
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
+    def __init__(self,):
         pass # You'll remove this when you fill out your constructor
+    
 
-    # What methods will you need?
+
+    
--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -1,10 +1,19 @@
 class ResaleShop:
 
     # What attributes will it need?
-
+    price: int 
+    inventory: list
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
     def __init__():
         pass # You'll remove this when you fill out your constructor
+     #  - buying a computer (add to inventory)
+    
+    #  - selling a computer (remove from inventory)
 
+
+    #  - refurbishing a computer
+
+    #  - updating a computer's price after refurbishment 
+    
     # What methods will you need?
