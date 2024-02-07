ce2c476890221ea19fd79b1f540e19958f3476e8
mikayledesp <mdespaigne@smith.edu>
2024-02-07 00:00:59 -0500

    Computer file is done, work needed on resale file

--- computer.py
+++ computer.py
@@ -1,19 +1,27 @@
 class Computer:
 
     # What attributes will it need?
-    #  - updating a computer's OS
-    operating_system: str
-    #  - storing information about a specific computer
     description: str
     processor_type: str
-    hard_drive_capacity: str 
-    memory:str 
+    hard_drive_capacity: int
+    memory: int
+    operating_system: str
     year_made: int
     price: int
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__(self,):
-        pass # You'll remove this when you fill out your constructor
-    
+    def __init__(self):   
+        self.description = "Mac Pro (Late 2013)"
+        self.processor_type = " Intel "
+        self.hard_drive_capacity = "256"
+        self.memory = "16"
+        self.operating_system = "High Sierra"
+        self.year_made = "19"
+        self.price = "1000"
 
-
-    
+computer = Computer()
+print("-" * 21)
+print("COMPUTER RESALE STORE")
+print("-" * 21)
+print("Buying", computer.description,computer.processor_type,computer.hard_drive_capacity, computer.memory,computer.operating_system, computer.year_made, computer.price )
+print("Adding to Inventory...")
+print("Done.")
--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -1,19 +1,30 @@
-class ResaleShop:
+import computer
+from typing import Dict, Optional
+inventory : Dict[int, Dict] = {}
+itemID = 0
 
+class ResaleShop:
     # What attributes will it need?
-    price: int 
-    inventory: list
+    computer.price: int 
+    
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
-     #  - buying a computer (add to inventory)
-    
+    def __init__(self,computer):
+        self.price = computer.price
+   
+    def add_computer(self, computer):
+        self.inventory.append(inventory)
+
+print(inventory)
+
+
+
     #  - selling a computer (remove from inventory)
 
+    #  - buying a computer (add to inventory)
 
     #  - refurbishing a computer
 
     #  - updating a computer's price after refurbishment 
     
-    # What methods will you need?
+
