74524a8c1c651e2bdee928ecbb180d0df42136bd
mikayledesp <mdespaigne@smith.edu>
2024-02-07 20:52:43 -0500

    Reset code

--- computer.py
+++ computer.py
@@ -1,5 +1,4 @@
-from oo_resale_shop import *
-class Computer:
+class Computer():
 
     # What attributes will it need?
     description: str
@@ -10,20 +9,31 @@ class Computer:
     year_made: int
     price: int
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__(self):   
-        self.description = "Mac Pro (Late 2013)"
-        self.processor_type = " Intel "
-        self.hard_drive_capacity = "256"
-        self.memory = "16"
-        self.operating_system = "High Sierra"
-        self.year_made = "19"
-        self.price = "1000"
+    def __init__(self, description, processor_type, hard_drive_capacity, memory, operating_system, year_made, price):   
+        self.description = description 
+        self.processor_type = processor_type
+        self.hard_drive_capacity = hard_drive_capacity
+        self.memory = memory
+        self.operating_system = operating_system
+        self.year_made = year_made
+        self.price = price
 
 computer = Computer()
 print("-" * 21)
 print("COMPUTER RESALE STORE")
 print("-" * 21)
-print("Buying", computer.description,computer.processor_type,computer.hard_drive_capacity, computer.memory,computer.operating_system, computer.year_made, computer.price )
+# print("Buying", computer.description,computer.processor_type,computer.hard_drive_capacity, computer.memory,computer.operating_system, computer.year_made, computer.price )
 print("Adding to Inventory...")
 print("Done.")
 print ("Checking Inventory......")
+
+ 
+# def main():
+#     comp_1 = Computer("2019 MacBook Pro", "Intel", "256", "16", "High Sierra", "2019" "1000")
+#     comp_2 = Computer( "Mac Pro (Late 2013)", "Intel","1024", "64", "MacOS Big Sur", "2013","1500")
+#     print("hello")
+
+# if __name__ == "__main__": main()
+
+# def buy(self, computer)
+    
--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -1,24 +1,32 @@
-from computer import *
+from computer import Computer
 from typing import Dict, Optional
 
 class ResaleShop:
     # What attributes will it need?
     inventory = []
-    
+    itemID = 0
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
     def __init__(self,computer):
         pass
+        
    
-    def buy(self, computer):
-         self.inventory.append(computer.description,computer.processor_type,computer.hard_drive_capacity, computer.memory,computer.operating_system, computer.year_made, computer.price )
-         print(self.inventory)
+    def buy(self, computer, inventory):
+         itemID += 1
+
+
     #  - selling a computer (remove from inventory)
     
     #  - buying a computer (add to inventory)
-    def update_price(self,computer, price):
-        pass
-
+    # def update_price(self,computer, price):
+    #     pass
+comp_1 = Computer("2019 MacBook Pro", "Intel", "256", "16", "High Sierra", "2019" "1000")
+comp_2 = Computer( "Mac Pro (Late 2013)", "Intel","1024", "64", "MacOS Big Sur", "2013","1500")
 
 
     #  - updating a computer's price after refurbishment 
+        
+def main():
+    print(comp_1)
+
+if __name__ == "__main__": main()
